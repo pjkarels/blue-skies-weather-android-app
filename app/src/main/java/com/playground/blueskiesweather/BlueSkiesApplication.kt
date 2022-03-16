d22a2c14343516194600a6db6b2f03bcb29c98bd
Paul Karels <pkarels@nerdery.com>
2022-03-15 20:57:03 -0500

    Turn Application into a Hilt Application

---
+++ app/src/main/java/com/playground/blueskiesweather/BlueSkiesApplication.kt
@@ -0,0 +1,8 @@
+package com.playground.blueskiesweather
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BlueSkiesApplication: Application() {
+}
